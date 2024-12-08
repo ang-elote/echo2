713667ffd54ae7f7a65e5c93c663d0682faf966e
ang-elote <angela.22chavez@gmail.com>
2024-12-08 13:42:32 -0600

    top bar 100% funcional finally :D

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,119 +1,84 @@
-import React, { useState } from "react";
-import { Drawer, IconButton, List, ListItem, ListItemText, Box, ListItemIcon } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChatRoundedIcon from '@mui/icons-material/ChatRounded';
-import LibraryMusicRoundedIcon from '@mui/icons-material/LibraryMusicRounded';
+import React from "react";
+import { Drawer, Box, List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
+import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
+import LibraryMusicRoundedIcon from "@mui/icons-material/LibraryMusicRounded";
 import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
-import PlaylistPlayRoundedIcon from '@mui/icons-material/PlaylistPlayRounded';
-import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
-import QueueMusicRoundedIcon from '@mui/icons-material/QueueMusicRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import PlaylistPlayRoundedIcon from "@mui/icons-material/PlaylistPlayRounded";
+import StarBorderRoundedIcon from "@mui/icons-material/StarBorderRounded";
+import QueueMusicRoundedIcon from "@mui/icons-material/QueueMusicRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
-const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDrawer = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
-      return;
-    }
-    setIsOpen(open);
-  };
-
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
-    <Box>
-      {/* Hamburger Button */}
-      <IconButton
-        onClick={toggleDrawer(true)}
-        sx={{ position: "fixed", top: "10px", left: "10px", color: "var(--blanco-roto)" }} // color de hamburger
-      >
-        <MenuIcon />
-      </IconButton>
-
-      {/* Drawer (Sidebar) */}
-      <Drawer
-        anchor="left"
-        open={isOpen}
-        onClose={toggleDrawer(false)}
+    <Drawer
+      anchor="left"
+      open={isOpen}
+      onClose={toggleSidebar}
+      sx={{
+        "& .MuiDrawer-paper": {
+          backgroundColor: "var(--pseudo-negro)", // color del fondo del sidebar
+          color: "var(--blanco-roto)", // color del texto del sidebar
+        },
+      }}
+    >
+      <Box
         sx={{
-          "& .MuiDrawer-paper": {
-            backgroundColor: "var(--pseudo-negro)", // Sidebar background color
-            color: "var(--blanco-roto)", // Sidebar text color
-          },
+          width: 250,
+          padding: 2,
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontSize: "1.4rem",
+          textTransform: "lowercase",
+          fontWeight: "bold"
         }}
+        role="presentation"
+        onClick={toggleSidebar}
       >
-        <Box
-          sx={{
-            width: 250,
-            padding: 2,
-            fontFamily: "Helvetica, Arial, sans-serif", // Sidebar fonts
-          }}
-          role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
-        >
-          <List>
-            {/* messages item */}
-            <ListItem button>
-              <ListItemIcon>
-                <ChatRoundedIcon sx={{ color: "var(--blanco-roto)" }} /> {/* Icon color */}
-              </ListItemIcon>
-              <ListItemText primary="messages"   sx={{ fontWeight: "bold",  fontSize: "1.4rem" }}  />
-            </ListItem>
-
-            {/* melodies Item */}
-            <ListItem button>
-              <ListItemIcon>
-                <LibraryMusicRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="melodies" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }} />
-            </ListItem>
-
-            {/* wishlist Item */}
-            <ListItem button>
-              <ListItemIcon>
-                <StarBorderRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="wishlist" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }}/>
-            </ListItem>
-
-            {/* playlists Item */}
-                <ListItem button>
-              <ListItemIcon>
-                <PlaylistPlayRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="playlists" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }} />
-            </ListItem>
-
-            {/* queue Item */}
-            <ListItem button>
-              <ListItemIcon>
-                <QueueMusicRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="queue" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }} />
-            </ListItem>
-
-
-
-             {/* settings Item */}
-             <ListItem button>
-              <ListItemIcon>
-                <SettingsRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="settings" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }} />
-            </ListItem>
-
-
-            {/* logout Item */}
-            <ListItem button>
-              <ListItemIcon>
-                <LogoutRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
-              </ListItemIcon>
-              <ListItemText primary="log out" sx={{ fontWeight: "bold",  fontSize: "1.4rem" }} />
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
-    </Box>
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <ChatRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Messages" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <LibraryMusicRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Melodies" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <StarBorderRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Wishlist" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <PlaylistPlayRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Playlists" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <QueueMusicRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Queue" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <SettingsRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <LogoutRoundedIcon sx={{ color: "var(--blanco-roto)" }} />
+            </ListItemIcon>
+            <ListItemText primary="Log Out" />
+          </ListItem>
+        </List>
+      </Box>
+    </Drawer>
   );
 };
 
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Box, IconButton, Typography, Badge } from "@mui/material";
-import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import Sidebar from "../components/Sidebar"; 
 
 const Topbar = ({ title = "Home" }) => {
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // control de la visibilidad del sidebar
 
+  // esconder o mostrar top bar al scroll
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
       if (currentScrollY > lastScrollY) {
-        // scroll abajo esconde topbar
-        setIsVisible(false);
+        setIsVisible(false); // esconder top bar al scroll down
       } else {
-        // scroll arriba muestra topbar
-        setIsVisible(true);
+        setIsVisible(true); // mostrar top bar al scroll up
       }
       setLastScrollY(currentScrollY);
     };
@@ -24,56 +25,73 @@ const Topbar = ({ title = "Home" }) => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [lastScrollY]);
 
+  // visibilidad de sidebar toggle
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: isVisible ? 0 : "-64px", // esconde barra
-        left: 0,
-        width: "100%",
-        height: "64px",
-        backgroundColor: "var(--pseudo-negro)",
-        color: "var(--blanco-roto)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "0 16px",
-        transition: "top 0.3s ease-in-out", // ani reaparición
-        zIndex: 1000,
-        boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
-        boxSizing: 'border-box', 
-        maxWidth: "100%", 
-      }}
-    >
-      {/* hamburger menu (sidebar) */}
-      <IconButton sx={{ color: "var(--blanco-roto)" }}>
-        <MenuRoundedIcon />
-      </IconButton>
-
-      {/* título */}
-      <Typography variant="h6" sx={{ fontFamily: "Rubik", fontWeight: "bold" }}>
-        {title}
-      </Typography>
-
-      {/* notifs */}
-      <Badge
-        badgeContent={6}  // The number of unread notifications
-        color="primary"  
+    <>
+      {/* Topbar */}
+      <Box
         sx={{
-          "& .MuiBadge-badge": {
-            backgroundColor: "var(--naranja-intenso)", // color d la notif
-            color: "var(--blanco-roto)", // color del texto dentro
-          },
+          position: "fixed",
+          top: isVisible ? 0 : "-64px", // esconderla al scroll
+          left: 0,
+          width: "100%",
+          height: "64px",
+          backgroundColor: "var(--pseudo-negro)",
+          color: "var(--blanco-roto)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "0 16px",
+          transition: "top 0.3s ease-in-out",
+          zIndex: 1000,
+          boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
+          boxSizing: "border-box",
         }}
       >
-        <NotificationsRoundedIcon 
-        sx={{ 
-            fontSize: 28,
-            color: "var(--blanco-roto)", 
-            marginLeft: "auto", 
-            fontFamily: "Rubik"}} />
-      </Badge>
-    </Box>
+        {/* hamburger */}
+        <IconButton
+          onClick={toggleSidebar}
+          sx={{
+            color: "var(--blanco-roto)",
+            visibility: isVisible ? "visible" : "hidden", // esconder al scroll
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* titulo */}
+        <Typography variant="h6" sx={{ fontFamily: "Rubik", fontWeight: "bold" }}>
+          {title}
+        </Typography>
+
+        {/* notif */}
+        <Badge
+          badgeContent={6}
+          color="primary"
+          sx={{
+            "& .MuiBadge-badge": {
+              backgroundColor: "var(--naranja-intenso)", 
+              color: "var(--blanco-roto)", 
+            },
+          }}
+        >
+          <NotificationsRoundedIcon
+            sx={{
+              fontSize: 28,
+              color: "var(--blanco-roto)",
+              fontFamily: "Rubik",
+            }}
+          />
+        </Badge>
+      </Box>
+
+      {/* sidebar */}
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+    </>
   );
 };
 
